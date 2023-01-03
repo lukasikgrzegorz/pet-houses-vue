cbc4823affdf9868479e195e16e30437d6c08fb4
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2023-01-03 22:00:52 +0100

    newItem form

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,26 @@ export default {
   components: {Logo},
   data(){
     return {
-      newItem: '',
+      houseType:"ground",
+      housePet:null,
+      housePrice:0,
       items: [
         { id: id++, type:"ground", pet: "cat", price: 20 },
         { id: id++, type:"ground", pet: "dog", price: 40 },
         { id: id++, type:"above", pet: "bird", price: 30 },
       ]
+    }
+  },
+  methods: {
+    addNewItem() {
+      if(this.houseType&&this.housePrice>0){
+        this.items.push({ id: id++, type: this.houseType, pet: this.housePet, price: this.housePrice });
+        this.houseType="ground";
+        this.housePet=null;
+        this.housePrice=0;
+      }else{
+        alert("Fill all fields!")
+      }
     }
   }
 };
@@ -22,7 +36,17 @@ export default {
 <template>
   <div id="app">
     <Logo/>
-   
+    <form @submit.prevent="addNewItem">
+      <div>
+        <input type="radio" id="ground" name="house_type" value="ground" v-model="houseType">
+        <label for="ground">Ground</label>
+        <input type="radio" id="above" name="house_type" value="above" v-model="houseType">
+        <label for="above">Above</label>
+      </div>
+      <input type="text" v-model="housePet" placeholder="Pet">
+      <input type="number" v-model="housePrice" placeholder="Price PLN">
+      <button>Add New Item</button>  
+    </form>
     <table>
       <tr>
         <th>Id</th>
@@ -50,6 +74,13 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+form{
+  margin: auto;
+  width: 500px;
+  padding: 20px;
+  background-color: red;
 }
 
 table{
