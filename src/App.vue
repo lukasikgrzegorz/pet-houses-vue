ea6b7585f2f6b4e2031730c31570422aaac91532
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2023-01-03 21:30:20 +0100

    logo component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,69 @@
+<script>
+import Logo from "./components/Logo.vue"
+
+let id=0;
+
+export default {
+  name: 'App',
+  components: {Logo},
+  data(){
+    return {
+      newItem: '',
+      items: [
+        { id: id++, type:"ground", pet: "cat", price: 20 },
+        { id: id++, type:"ground", pet: "dog", price: 40 },
+        { id: id++, type:"above", pet: "bird", price: 30 },
+      ]
+    }
+  }
+};
+</script>
+
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <Logo/>
+   
+    <table>
+      <tr>
+        <th>Id</th>
+        <th>Type</th>
+        <th>Pet</th>
+        <th>Price</th>
+      </tr>
+      <tr v-for="item in items" :key="item.id">
+        <td>{{item.id}}</td>
+        <td>{{item.type}}</td>
+        <td>{{item.pet}}</td>
+        <td>{{item.price}}</td>
+      </tr>
+    </table>
+   
   </div>
 </template>
 
-<script>
-export default {
-  name: 'App'
-}
-</script>
+
 
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
+
+table{
+  margin: auto;
+}
+
+td{
+  width: 100px;
+  text-align: center;
+}
+
+th{
+  background: brown;
+  color: white;
+}
+
 </style>
---
+++ src/components/Logo.vue
@@ -0,0 +1,28 @@
+<template>
+   <div class="logo">Pet Houses</div>
+</template>
+
+
+<script>
+export default {
+  name: 'Logo'
+}
+</script>
+
+<style>
+.logo{
+  font-weight: 700;
+  font-size: 40px;
+  padding: 15px;
+  border: 4px dotted black;
+  width: max-content;
+  margin: auto;
+  transform: rotate(5deg);
+  transition: transform 500ms linear;
+  margin-bottom: 40px;
+}
+
+.logo:hover{
+  transform: rotate(0);  
+}
+</style>
